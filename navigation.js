0b0c8711c2fa8d8f7105050681abfa39124c6a18
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2024-05-23 00:01:15 -0500

    parallax achieved

--- navigation.js
+++ navigation.js
@@ -4,17 +4,39 @@ document.addEventListener('DOMContentLoaded', function () {
     function openNavPage() {
         if (page) {
             page.style.right = "50vw";
-            page.style.overflow
         } else {
-            console.error('element not found')
+            console.error("element not found");
         }
     }
 
-    let menuIcon = document.querySelector('.menu-icon-b');
-    if (menuIcon) {
-        menuIcon.addEventListener('click', openNavPage);
-    } else {
-        console.error('menu icon not found')
+    function closeNavPage() {
+        if (page) {
+            page.style.right = "0vw";
+        } else {
+            console.error("element not found");
+        }
     }
 
+    let menuIcon = document.querySelector(".menu-icon-b");
+    if (menuIcon) {
+        menuIcon.addEventListener("click", openNavPage);
+    } else {
+        console.error("menu icon not found");
+    }
+
+    let homeButton = document.querySelector("#home-button");
+    if (homeButton) {
+        homeButton.addEventListener("click", closeNavPage);
+    } else {
+        console.error("home button not found");
+    }
+
+});
+
+window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    const parallaxOffset = scrollPosition * 0.65;
+
+    // Apply the parallax effect to the image
+    document.querySelector('.img-home').style.transform = `translateY(${parallaxOffset}px)`;
 });
