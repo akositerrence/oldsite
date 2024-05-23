05ac996093618dc6a76cbba8caa4f8b0adaf7dce
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2024-05-22 23:03:27 -0500

    menu button works!!!

--- navigation.js
+++ navigation.js
@@ -1,5 +1,19 @@
-let page = document.getElementsByClassName("home-screen-container");
+document.addEventListener('DOMContentLoaded', function () {
+    let page = document.getElementsByClassName("home-screen-container")[0];
 
-function openNavPage() {
-    page.style.right = "50vw";
-}
+    function openNavPage() {
+        if (page) {
+            page.style.right = "50vw";
+        } else {
+            console.error('element not found')
+        }
+    }
+
+    let menuIcon = document.querySelector('.menu-icon-b');
+    if (menuIcon) {
+        menuIcon.addEventListener('click', openNavPage);
+    } else {
+        console.error('menu icon not found')
+    }
+
+});
