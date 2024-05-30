c5498d0209e9ba94e44347d8c5621c5fd6fc13d9
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2024-05-30 18:42:38 -0500

    map changes

--- javascript/effects.js
+++ javascript/effects.js
@@ -19,13 +19,13 @@ document.addEventListener('DOMContentLoaded', function () {
         var navMorse = navPage.querySelector(".nav-morse-container");
         var navButtons = navPage.querySelector(".off-screen-buttons");
 
-        headTopic.style.borderRadius = `${parallaxOffset * 0.3}vw`;
+        headTopic.style.borderRadius = `${parallaxOffset * 0.2}vw`;
         navScroll.style.transform = `translateY(${scrollOffset}px)`;
         navMorse.style.transform = `translateY(${morseOffset}px)`;
         navButtons.style.transform = `translateY(${buttonOffset}px)`;
         imgHome.style.transform = `translateY(${parallaxOffset}px)`;
-        imgHome.style.borderTopRightRadius = `${parallaxOffset * 0.7}vw`;
-        imgHome.style.borderTopLeftRadius = `${parallaxOffset * 0.7}vw`;
+        imgHome.style.borderTopRightRadius = `${parallaxOffset * 0.6}vw`;
+        imgHome.style.borderTopLeftRadius = `${parallaxOffset * 0.6}vw`;
         fadedName.style.opacity = Math.max(0, ((fadeEnd - scrollPosition) / fadeEnd));
         fadedName.style.transform = `translateY(${headerOffset}px)`;
     });
--- javascript/map.js
+++ javascript/map.js
@@ -23,6 +23,9 @@ document.addEventListener("DOMContentLoaded", function () {
             leftButton = mapAccess.querySelector("#left");
             rightButton = mapAccess.querySelector("#right");
             philippineText = mapAccess.querySelector(".philippines");
+            singaporeText = mapAccess.querySelector(".singapore");
+            bismarckText = mapAccess.querySelector(".bismarck");
+            fargoText = mapAccess.querySelector(".fargo");
 
             function all() {
                 map.style.transform = "scale(1)"
@@ -31,21 +34,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
             function sanMiguel() {
                 map.style.transform = "scale(6) translateY(-2vw) translateX(-32vw)"
-                philippineText.style.opacity = "80%";
+                philippineText.style.opacity = "90%";
+                singaporeText.style.opacity = "0%";
             }
 
             function singapore() {
                 map.style.transform = "scale(6.5) translateY(-5vw) translateX(-24vw)"
                 philippineText.style.opacity = "0%";
-                philippineText.style.transform = "translateX(3vw)";
+                singaporeText.style.opacity = "90%";
+                bismarckText.style.opacity = "0%";
             }
 
             function bismarck() {
                 map.style.transform = "scale(4) translateY(7vw) translateX(32vw)"
+                singaporeText.style.opacity = "0%";
+                bismarckText.style.opacity = "90%";
+                fargoText.style.opacity = "0%";
             }
 
             function fargo() {
-                map.style.transform = "scale(4.25) translateY(7vw) translateX(27vw)"
+                map.style.transform = "scale(4.25) translateY(3vw) translateX(20vw)"
+                bismarckText.style.opacity = "0%";
+                fargoText.style.opacity = "90%";
             }
 
             function positionSwitch(position) {
